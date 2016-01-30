7ea26a1f80c06886a996fb7da4ec2a0ee296d689
belithioben <benjaminatbi@gmail.com>
2016-01-30 12:10:39 -0800

    Lazers and enemies have color

---
+++ Assets/LazerBehavior.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LazerBehavior : MonoBehaviour {
+
+    public Note Note;
+
+    void Start()
+    {
+        GetComponent<SpriteRenderer>().color = Notes.EntityColor[Note];
+    }
+
+}
