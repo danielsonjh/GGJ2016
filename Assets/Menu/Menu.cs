50e0b4d00113144980f4ff9bb0481a11072027de
Daniel Son <danielsonjh@hotmail.com>
2016-02-04 21:14:57 -0800

    Added credits + instructions.

--- Assets/Menu/Menu.cs
+++ Assets/Menu/Menu.cs
@@ -5,10 +5,19 @@ using UnityEngine.SceneManagement;
 public class Menu : MonoBehaviour
 {
     private CanvasFader _canvasFader;
+    private GameObject _creditsPanel;
+    private GameObject _instructionsPanel;
 
     void Start()
     {
         _canvasFader = GameObject.Find("ScreenFader").GetComponent<CanvasFader>();
+        if (Application.loadedLevelName == "Menu")
+        {
+            _creditsPanel = GameObject.Find("CreditsPanel");
+            _creditsPanel.SetActive(false);
+            _instructionsPanel = GameObject.Find("InstructionsPanel");
+            _instructionsPanel.SetActive(false);
+        }
     }
 
     public void StartGame(int level)
@@ -23,6 +32,26 @@ public class Menu : MonoBehaviour
     {
         _canvasFader.FadeIn();
         StartCoroutine(StartScene("Menu"));
+    }
+
+    public void OpenInstructionsPanel()
+    {
+        _instructionsPanel.SetActive(true);
+    }
+
+    public void CloseInstructionsPanel()
+    {
+        _instructionsPanel.SetActive(false);
+    }
+
+    public void OpenCreditsPanel()
+    {
+        _creditsPanel.SetActive(true);
+    }
+
+    public void CloseCreditsPanel()
+    {
+        _creditsPanel.SetActive(false);
     }
 
     IEnumerator StartScene(string sceneName)
